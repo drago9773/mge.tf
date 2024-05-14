07d480ee61e38d36642bb4e02147d875aaa0cbb1
tommy-mor <thmorriss@gmail.com>
2024-05-14 13:12:42 -0400

    added sessions and login

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require("body-parser");
+const SteamAuth = require("node-steam-openid");
+const session = require('express-session');
+
 
 
 const app = express();
@@ -14,11 +17,45 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+	secret: 'your-secret-key', // Replace with a secure secret key
+	resave: false,
+	saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', { req: req });
 });
 
+const API_KEY = '2C7E4CDF46C4D4FB5875A8E6E040BFC0';
+
+const steam = new SteamAuth({
+	realm: 'http://localhost:3005/',
+	returnUrl: 'http://localhost:3005/verify',
+	apiKey: API_KEY,
+});
+
+app.get('/init-openid', async (req, res) => {
+	const redirectUrl = await steam.getRedirectUrl();
+	return res.redirect(redirectUrl);
+});
+
+app.get('/verify', async (req, res) => {
+	try {
+		const user = await steam.authenticate(req);
+		req.session.user = user;
+	} catch {
+	}
+	return res.redirect('/');
+});
+app.get('/dashboard', (req, res) => {
+	res.render('dashboard', { session: req.session });
+});
+
+app.get('/logout', (req, res) => {
+	res.session = null;
+	return res.redirect('/');
+});
 const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
