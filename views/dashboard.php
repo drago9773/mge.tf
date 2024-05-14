1bacfbea120c41a9b3d452b9c887a47ee918a10a
tommy-mor <thmorriss@gmail.com>
2024-05-14 12:46:03 -0400

    progress

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!$_SESSION['logged_in']){
+if (!$_SESSION['logged_in']) {
     header("location: error.php");
     exit();
 }
@@ -12,18 +12,20 @@ $steamID = $_SESSION['userData']['steam_id'];
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php include 'header.php'; ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="dashboard.css">
 </head>
+
 <body>
     <div class="dashboard-container">
         <h1>Welcome to the dashboard, <?php echo $username; ?></h1>
         <img src='<?php echo $avatar; ?>' alt="Avatar">
         <p>
-            Steam Profile: 
+            Steam Profile:
             <a href="https://steamcommunity.com/profiles/<?php echo $steamID; ?>" target="_blank">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Steam_Logo.png" alt="Steam Logo">
             </a>
@@ -31,7 +33,8 @@ $steamID = $_SESSION['userData']['steam_id'];
         <p>
             Logs.tf Profile:
             <a href="https://logs.tf/profile/<?php echo $steamID; ?>" target="_blank">
-                <img src="https://avatars.cloudflare.steamstatic.com/7bcc7b08e91659863bdbff2acf47ef5a25e9c3e9_full.jpg" alt="Logs.tf Logo">
+                <img src="https://avatars.cloudflare.steamstatic.com/7bcc7b08e91659863bdbff2acf47ef5a25e9c3e9_full.jpg"
+                    alt="Logs.tf Logo">
             </a>
         </p>
         <p>
@@ -43,4 +46,5 @@ $steamID = $_SESSION['userData']['steam_id'];
         <a href="logout.php">Logout</a>
     </div>
 </body>
+
 </html>
