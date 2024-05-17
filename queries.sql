af1bf5601cda404d82e3ec9155714caf92b4b0a9
Neptune <Jonathan.Birchman@gmail.com>
2024-05-17 16:31:15 -0700

    .

--- queries.sql
+++ queries.sql
@@ -1,31 +1,13 @@
--- Create the users table with steam_id and steam_username
+-- create user database 
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     steam_id TEXT NOT NULL,
     steam_username TEXT NOT NULL,
-    steam_avatar TEXT -- Store the profile picture data as a BLOB (Binary Large Object)
-);
--- Create the forums table
-CREATE TABLE IF NOT EXISTS forums (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL
-);
+    steam_avatar TEXT
+)
 
--- Create the messages table
-CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT NOT NULL
-);
+-- insert steam ID, steam username, and steam avatar
+INSERT INTO users (steam_id, steam_username, steam_avatar) VALUES ('steam ID', 'steam username', 'https://avatars.cloudflare.steamstatic.com/6539ac9458157152a788bf01b935a7287b300337_full.jpg');
 
--- Insert steam_id and steam_username into the users table
-INSERT INTO users (steam_id, steam_username, steam_avatar) VALUES ('example_steam_id', 'example_username', 'https://avatars.cloudflare.steamstatic.com/6539ac9458157152a788bf01b935a7287b300337_full.jpg');
-
--- Insert forum into the forums table
-INSERT INTO forums (title, description) VALUES ('Example Forum', 'This is an example forum description.');
-
--- Select all usernames from the users table
+-- view all steam usernames
 SELECT steam_username FROM users;
-
--- Select all forums from the forums table
-SELECT title, description FROM forums;
