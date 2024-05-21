6eac00d053d9f3ef77dae15f637392118e1b7ed2
tommy-mor <thmorriss@gmail.com>
2024-05-21 16:19:27 -0400

    basic forum/thread structure

--- app.js
+++ app.js
@@ -22,44 +22,37 @@ app.use(session({
     saveUninitialized: true
 }));
 // user database
-const users_db = new sqlite3.Database('users.db', sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database('users.db', sqlite3.OPEN_READWRITE, async (err) => {
     if (err) {
         console.error(err.message);
-    } else {
-        users_db.run(`CREATE TABLE IF NOT EXISTS users (
+    }
+    await db.run(`CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             steam_id TEXT NOT NULL,
-            steam_username TEXT NOT NULL
-        )`, (err) => {
-            if (err) {
-                console.error("Error creating users table: " + err.message);
-            }
-        });
-    }
-});
-// forums database
-const forums_db = new sqlite3.Database('forums.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        forums_db.run(`CREATE TABLE IF NOT EXISTS forums (
-            forums_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            steam_id TEXT NOT NULL,
-            steam_username TEXT NOT NULL,
-            steam_avatar TEXT
-            title TEXT NOT NULL,
-            content TEXT NOT NULL,
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (steam_id) REFERENCES users(steam_id)
-        )`, (err) => {
-            if (err) {
-                console.error("Error creating forums table: " + err.message);
-            }
-        });
-    }
+            steam_username TEXT NOT NULL)`);
+
+    await db.run(`CREATE TABLE IF NOT EXISTS threads (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  title TEXT NOT NULL,
+                  content TEXT NOT NULL,
+                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                  bumped_at TIMESTAMP DEFAULT CURENT_TIMESTAMP,
+
+                  owner INTEGER NOT NULL,
+                  FOREIGN KEY (owner) REFERENCES users(id))`);
+
+    await db.run(`CREATE TABLE IF NOT EXISTS posts (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  content TEXT NOT NULL,
+                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+                  thread INTEGER NOT NULL,
+                  owner INTEGER NOT NULL,
+                  FOREIGN KEY (owner) REFERENCES users(id)
+                  FOREIGN KEY (thread) REFERENCES thread(id))`);
 });
 
-app.post('/postContent', (req, res) => {
+app.post('/postContent', async (req, res) => {
     const { content, title } = req.body;
     const user = req.session.user;
 
@@ -71,42 +64,86 @@ app.post('/postContent', (req, res) => {
         return res.status(400).json({ error: 'Content and title are required' });
     }
 
-    forums_db.run('INSERT INTO forums (steam_id, steam_username, steam_avatar, title, content, created_at) VALUES (?, ?, ?, ?, ?, ?)', 
-        [user.steamid, user.username, user.avatar.small, title, content, new Date().toISOString().slice(0, 16)], 
-        (err) => {
-            if (err) {
-                console.error(err.message);
-                return res.status(500).json({ error: 'Failed to post content.' });
-            } else {
-                console.log(`Forum Post "${content}" saved to the database.`);
-                return res.status(200).json({ message: 'Post saved successfully.' });
-            }
+    console.log(user);
+    db.get('SELECT id FROM users WHERE steam_id = ?', [user.steamid], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: 'Failed to post content.' });
         }
-    );
+        const user_id = row.id;
+        db.run('INSERT INTO threads (title, content, owner) VALUES (?, ?, ?)',
+            [title, content, user_id],
+            (err) => {
+                if (err) {
+                    console.error(err.message);
+                    return res.status(500).json({ error: 'Failed to post content.' });
+                } else {
+                    console.log(`Forum Post "${content}" saved to the database.`);
+                    return res.status(200).redirect('/forums');
+                }
+            }
+        );
+    });
 });
 
 app.get('/forums', (req, res) => {
-    const sql = 'SELECT forums_id, steam_id, steam_username, steam_avatar, title, content, created_at FROM forums';
-    forums_db.all(sql, [], (err, rows) => {
+    const sql = 'SELECT threads.id as thread_id, * FROM threads LEFT JOIN users ON threads.owner = users.id ORDER BY bumped_at DESC';
+    db.all(sql, [], (err, rows) => {
         if (err) {
             console.error(err.message);
             res.status(500).json({ error: 'Failed to fetch post.' });
         } else {
-            res.render('forums', { forums: rows });
+            res.render('forums', { forums: rows, session: req.session });
         }
     });
 });
 
-app.post('/remove_post', (req, res) => {
-    const postId = req.body.post_id; 
+app.post('/posts', (req, res) => {
+    const { content, thread_id } = req.body;
+    const user = req.session.user;
 
-    const sql = 'DELETE FROM forums WHERE forums_id = ?';
-    forums_db.run(sql, [postId], (err) => {
+    console.log(req.body);
+
+    if (!user) {
+        return res.status(401).json({ error: 'Login to create post' });
+    }
+
+    if (!content) {
+        return res.status(400).json({ error: 'Content is required' });
+    }
+
+    db.get('SELECT id FROM users WHERE steam_id = ?', [user.steamid], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: 'Failed to post content.' });
+        }
+        const user_id = row.id;
+        db.run('INSERT INTO posts (content, thread, owner) VALUES (?, ?, ?)',
+            [content, thread_id, user_id],
+            (err) => {
+                if (err) {
+                    console.error(err.message);
+                    return res.status(500).json({ error: 'Failed to post content.' });
+                } else {
+                    console.log(`Forum Post "${content}" saved to the database.`);
+                    return res.status(200).redirect(`/post/${thread_id}`);
+                }
+            }
+        );
+    });
+});
+
+app.post('/remove_post', (req, res) => {
+    const postId = req.body.post_id;
+    console.log('postid', postId);
+
+    const sql = 'DELETE FROM threads WHERE id = ?';
+    db.run(sql, [postId], (err) => {
         if (err) {
             console.error(err.message);
             res.status(500).json({ error: 'Failed to remove post.' });
         } else {
-            res.redirect('/'); 
+            res.redirect('/forums');
         }
     });
 });
@@ -118,7 +155,7 @@ const eloDb = new sqlite3.Database('sourcemod-local.sq3', (err) => {
 });
 
 app.get('/', (req, res) => {
-    const result = eloDb.all('SELECT * FROM mgemod_stats ORDER BY rating DESC', [], (err, rows) => {
+    eloDb.all('SELECT * FROM mgemod_stats ORDER BY rating DESC', [], (err, rows) => {
         if (err) {
             console.error("Error querying database: " + err.message);
             res.status(500).send("Internal Server Error");
@@ -144,32 +181,32 @@ app.get('/init-openid', async (req, res) => {
 });
 
 app.get('/verify', async (req, res) => {
-	try {
-		const user = await steam.authenticate(req);
-		req.session.user = user;
-		
-		// check if first time login
-		users_db.get('SELECT * FROM users WHERE steam_id = ?', [user.steamid], (err, row) => {
-			if (err) {
-				console.error("Error querying database: " + err.message);
-			} else if (!row) {
-				// add if first time
-				users_db.run('INSERT INTO users (steam_id, steam_username, steam_avatar) VALUES (?, ?, ?)', [user.steamid, user.username, user.avatar.small], (err) => {
-					if (err) {
-						console.error("Error inserting into database: " + err.message);
-					}
-				});
-			}
-		});
-	} catch (err) {
-		console.error("Authentication error: " + err.message);
-	}
-	return res.redirect('/');
+    try {
+        const user = await steam.authenticate(req);
+        req.session.user = user;
+
+        // check if first time login
+        db.get('SELECT * FROM users WHERE steam_id = ?', [user.steamid], (err, row) => {
+            if (err) {
+                console.error("Error querying database: " + err.message);
+            } else if (!row) {
+                // add if first time
+                db.run('INSERT INTO users (steam_id, steam_username, steam_avatar) VALUES (?, ?, ?)', [user.steamid, user.username, user.avatar.small], (err) => {
+                    if (err) {
+                        console.error("Error inserting into database: " + err.message);
+                    }
+                });
+            }
+        });
+    } catch (err) {
+        console.error("Authentication error: " + err.message);
+    }
+    return res.redirect('/');
 });
 
 app.get('/player_page/:steamid', (req, res) => {
     const steamid = req.params.steamid;
-    users_db.get('SELECT * FROM users WHERE steam_id = ?', [steamid], (err, row) => {
+    db.get('SELECT * FROM users WHERE steam_id = ?', [steamid], (err, row) => {
         if (err) {
             console.error("Error querying database: " + err.message);
             res.status(500).send("Internal Server Error");
@@ -183,18 +220,25 @@ app.get('/player_page/:steamid', (req, res) => {
 });
 app.get('/post/:forumid', (req, res) => {
     const forumid = req.params.forumid;
-    forums_db.get('SELECT * FROM forums WHERE forums_id = ?', [forumid], (err, row) => {
+    db.get('SELECT threads.id as thread_id, * FROM threads LEFT JOIN users ON threads.owner = users.id WHERE threads.id = ?', [forumid], (err, row) => {
         if (err) {
             console.error("Error querying database: " + err.message);
             res.status(500).send("Internal Server Error");
         } else {
-            res.render('post', { forum: row });
+            db.all('SELECT * FROM posts LEFT JOIN users ON posts.owner = users.id WHERE thread = ?', [forumid], (err, rows) => {
+                if (err) {
+                    console.error("Error querying database: " + err.message);
+                    res.status(500).send("Internal Server Error");
+                } else {
+                    res.render('posts', { posts: rows, thread: row });
+                }
+            });
         }
     });
 });
 
 app.get('/users', (req, res) => {
-    users_db.all('SELECT * FROM users', [], (err, rows) => {
+    db.all('SELECT * FROM users', [], (err, rows) => {
         if (err) {
             console.error("Error querying database: " + err.message);
             res.status(500).send("Internal Server Error");
