7464217b163730b0d81337eab6e3ba1342c33247
Neptune <Jonathan.Birchman@gmail.com>
2024-05-31 10:04:40 -0700

    developing search bar for elo

---
+++ views/js/main.js
@@ -0,0 +1,23 @@
+function filterUsers() {
+    var input, filter, usersDisplay, users, username, steamId, i, usernameTxt, steamIdTxt;
+    input = document.getElementById('eloSearch');
+    filter = input.value.toLowerCase();
+    usersDisplay = document.getElementsByClassName('users_display')[0];
+    users = usersDisplay.getElementsByClassName('users_container');
+
+    for (i = 0; i < users.length; i++) {
+        username = users[i].getElementsByClassName('users_username')[0];
+        steamId = users[i].getElementsByClassName('users_steam_id')[0];
+        usernameTxt = username.textContent || username.innerText;
+        steamIdTxt = steamId.textContent || steamId.innerText;
+        
+        if (usernameTxt.toLowerCase().indexOf(filter) > -1 || steamIdTxt.indexOf(filter) > -1) {
+            users[i].style.display = "";
+        } else {
+            users[i].style.display = "none";
+        }
+    }
+}
+document.getElementById("userSearch").addEventListener("input", function() {
+    filterUsers();
+});
