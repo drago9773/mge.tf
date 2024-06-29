2e080b4e35f4be61502ed19036af069518a0f18e
EternalWill43 <haler43@icloud.com>
2024-06-29 11:54:47 -0400

    feat: tailwind styling for users

--- views/js/users.js
+++ views/js/users.js
@@ -1,15 +1,14 @@
 function filterUsers() {
-    var input, filter, usersDisplay, users, username, steamId, i, usernameTxt, steamIdTxt;
-    input = document.getElementById('userSearch');
-    filter = input.value.toLowerCase();
-    usersDisplay = document.getElementsByClassName('users_display')[0];
-    users = usersDisplay.getElementsByClassName('users_container');
+    const input = document.getElementById('userSearch');
+    let filter = input.value.toLowerCase();
+    let users = document.getElementById('users_container').children;
 
-    for (i = 0; i < users.length; i++) {
-        username = users[i].getElementsByClassName('users_username')[0];
-        steamId = users[i].getElementsByClassName('users_steam_id')[0];
-        usernameTxt = username.textContent || username.innerText;
-        steamIdTxt = steamId.textContent || steamId.innerText;
+    for (let i = 0; i < users.length; i++) {
+        const username = users[i].querySelector('#users_username');
+        const steamId = users[i].querySelector('#users_steam_id');
+        if (!username && !steamId) continue;
+        const usernameTxt = username.textContent || username.innerText;
+        const steamIdTxt = steamId.textContent || steamId.innerText;
 
         if (usernameTxt.toLowerCase().indexOf(filter) > -1 || steamIdTxt.indexOf(filter) > -1) {
             users[i].style.display = '';
