538f2687272d8edb1a8fb6dd29512fa231da8fc6
EternalWill43 <haler43@icloud.com>
2024-07-02 11:11:57 -0400

    build: have tailwind scan js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./views/**/*.{ejs,html}'],
+  content: ['./views/**/*.{js,ejs,html}'],
   theme: {
     extend: {
       textShadow: {
